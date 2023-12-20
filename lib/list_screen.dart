f8c0eafbf921ca6230a3f4507ebb3e32bc393deb
ssd72 <sungdae.yi@gmail.com>
2023-12-20 12:47:36 +0100

    Todo list 만들기 - DB의 데이터 표시하기

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/create_screen.dart';
-import 'package:todolist/todo.dart';
+
+import 'main.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -10,20 +11,6 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final todos = [
-    Todo(
-      title: 'title 1',
-      dateTime: 12412,
-    ),
-    Todo(
-      title: 'title 2',
-      dateTime: 456456,
-    ),
-    Todo(
-      title: 'title 3',
-      dateTime: 3245345345,
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +19,7 @@ class _ListScreenState extends State<ListScreen> {
         title: const Text('Todo 리스트'),
       ),
       body: ListView(
-        children: todos
+        children: todos.values
             .map(
               (todo) => ListTile(
                 title: Text(todo.title),
